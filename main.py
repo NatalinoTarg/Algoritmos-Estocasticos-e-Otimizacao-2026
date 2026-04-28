dd0e7aa347f24fb7407bafd896e29c4adc4a31f7
Lukas Vieira <lukastarginovieira@gmail.com>
2026-04-28 23:37:22 +0100

    Derivadas de segundo Grau

--- main.py
+++ main.py
@@ -27,20 +27,20 @@ def ErroRelativo(aproximação:float, exato:float, absoluto:float=None):
         exato = math.pow(10,-8)
     return absoluto/exato
 
-def derivada_aprox(valor:float | list, func:callable):
+def derivada_aprox(valor:float | list, func:callable, passo: int = 8):
     retlist = True
     if(not isinstance(valor, list)):
         valor = [valor]
         retlist = False
-    passo_x = 0.00000001
+    passo_x = math.pow(10,-passo)
     for i in range(len(valor)):
         valor[i] = std_round(valor[i])
     deriv = []
     for i in range(len(valor)):
         args_backward = valor[:]
         args_forward = valor[:]
-        args_forward[i] = valor[i] + passo_x
-        args_backward[i] = valor[i] - passo_x
+        args_forward[i] = std_round(valor[i] + passo_x)
+        args_backward[i] = std_round(valor[i] - passo_x)
         deriv.append(std_round((std_round(func(*args_forward)) - std_round(func(*args_backward))) / (2 * passo_x)))
     if retlist:
         return deriv
@@ -48,6 +48,17 @@ def derivada_aprox(valor:float | list, func:callable):
         return deriv[0]
 
 """print(derivada_aprox([2,1],lambda x, y: math.pow(x,2) + x*y))"""
+
+def seg_derivada_aprox(valor:float, func:callable,  passo: int = 4):
+    valor = std_round(valor)
+    passo_x = math.pow(10,-passo)
+    arr = (std_round(func(valor-passo_x)) - std_round(2*func(valor)) + std_round(func(valor+passo_x)))
+    passoSqr = std_round(passo_x**2)
+    passoDoze = std_round(passoSqr/12)
+    arr = std_round(arr/passoSqr)
+    return std_round(arr - passoDoze)
+
+print(seg_derivada_aprox(5, lambda x: x**2 + 4*x +4))
 
 def gradiente(ingreme:bool, iteracoes:int, valor_inicial:float, passo:float, func:callable):
     valor = std_round(valor_inicial)
@@ -61,16 +72,34 @@ gradiente(False, 5, 5, 0.1, lambda x: math.pow(x,2) + 4*x +4)
 gradiente(False, 100, 5, 0.1, lambda x: math.pow(x,2) + 4*x +4)
 gradiente(False, 100, -5, 0.1, lambda x: math.pow(x,2) + 4*x +4)"""
 
-while (True):
-    try:
-        print("Argumentos(x, y, z, ... n)")
-        arg = input()
-        print("Função")
-        func = input()
-        inp = f"lambda {arg}: {func}"
-        funcao = safe_eval(inp)
-        funcao(*range(len(arg)))
-        break
-    except Exception as ex:
-        print("Função invalida!",ex)
-print(inp, derivada_aprox(5, funcao), funcao(5))
+def getLambdaInput() -> callable:
+    funcao = lambda : None
+    while (True):
+        try:
+            print("Argumentos(x, y, z, ... n)")
+            arg = input()
+            print("Função")
+            func = input()
+            inp = f"lambda {arg}: {func}"
+            funcao = safe_eval(inp)
+            arg = re.split(",\\s?", arg)
+            funcao(*range(len(arg)))
+            break
+        except Exception as ex:
+            print("Função invalida!",ex)
+    return funcao
+
+def newton(iteracoes:int, valor_inicial:float, func:callable):
+    valor = std_round(valor_inicial)
+    for i in range (1, iteracoes+1):
+        segDeriv = seg_derivada_aprox(valor, func)
+        if (segDeriv == 0):
+            segDeriv = math.pow(10,-8)
+        valor = std_round(valor - std_round(
+            std_round(derivada_aprox(valor, func)) /
+            segDeriv)
+        )
+        print_iter(i, valor)
+    return valor
+
+newton(iteracoes=5, valor_inicial=5, func=lambda x: x**2 + 4*x +4)
